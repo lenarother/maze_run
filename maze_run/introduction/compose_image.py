bdd4851e208dfd1b4291114d24a0ba016cc36240
lenarother <rother.magdalena@gmail.com>
2016-02-26 12:29:48 +0100

    Review introduction script

--- maze_run/introduction/compose_image.py
+++ maze_run/introduction/compose_image.py
@@ -1,3 +1,8 @@
+# Review by MR 26.03.16
+# Nice to have such a short script for the beginnging
+# I would rather go for full words instead of abbreviations for clarity
+# This is what pylint says: Your code has been rated at -4.00/10
+# Could not execute because of missing file.
 
 from pygame import image, Rect
 
